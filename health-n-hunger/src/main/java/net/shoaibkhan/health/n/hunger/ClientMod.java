027095925c434c12f09237712c327a7d3979ddfa
shoaib11120 <mdshoaibkhan.1999@gmail.com>
2021-02-16 17:53:47 +0530

    Fixed some bugs

--- health-n-hunger/src/main/java/net/shoaibkhan/health/n/hunger/ClientMod.java
+++ health-n-hunger/src/main/java/net/shoaibkhan/health/n/hunger/ClientMod.java
@@ -1,72 +1,65 @@
 package net.shoaibkhan.health.n.hunger;
 
-
 import org.lwjgl.glfw.GLFW;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.LiteralText;
+import net.minecraft.util.Util;
 
-public class ClientMod implements ClientModInitializer {
-    public static KeyBinding kb;
-    private static float tickCount = 0f;
-    @Override
-    public void onInitializeClient() {
-        kb = KeyBindingHelper
-                .registerKeyBinding(new KeyBinding("Check", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "Health n Hunger"));
-        
-        // ClientTickEvents.END_CLIENT_TICK.register(client -> {
-        //     while (kb.wasPressed()) {
-        //         // client.player.sendChatMessage("st" + client.player.getHealth());
-        //         // TextRenderer textRenderer = client.getInstance().textRenderer;
+@Environment(EnvType.CLIENT)
+public class ClientMod  {
+    private MinecraftClient client;
+    private float tickCount=0f;
+    
+    
 
-        //     }
-        // });
-
-        HudRenderCallback.EVENT.register(new HudRenderCallback() {
-
-            @Override
-            public void onHudRender(MatrixStack matrixStack, float tickDelta) {
-                // TODO Auto-generated method stub
-                if(tickCount>0f){
-                    TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
-                    double health = MinecraftClient.getInstance().player.getHealth();
-                    double hunger = MinecraftClient.getInstance().player.getHungerManager().getFoodLevel();
-                    int height = MinecraftClient.getInstance().getWindow().getScaledHeight();
-                    int width = MinecraftClient.getInstance().getWindow().getScaledWidth();
-                    
-                    matrixStack.push();
-                    matrixStack.scale(2, 2, 1);
-                    
-                    DrawableHelper.drawTextWithShadow(matrixStack, textRenderer, new LiteralText(""+health/2+"X Health    "+hunger/2+"X Food Level"),(int)(width*0.1) , (int)(height*0.35), 0xff0000);
-                    matrixStack.pop();
+    public ClientMod(KeyBinding kb){
+        client = MinecraftClient.getInstance();
+        HudRenderCallback.EVENT.register((__, ___)->{
+            if(tickCount>0f){
+                if (this.kbPressed()) {
                     tickCount--;
                 }
-                while(kb.wasPressed()){
-                    // MinecraftClient.getInstance().player.sendSystemMessage(new LiteralText("string"), Util.NIL_UUID);
-                    TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
-                    double health = MinecraftClient.getInstance().player.getHealth();
-                    double hunger = MinecraftClient.getInstance().player.getHungerManager().getFoodLevel();
-                    int height = MinecraftClient.getInstance().getWindow().getScaledHeight();
-                    int width = MinecraftClient.getInstance().getWindow().getScaledWidth();
-                    matrixStack.push();
-                    matrixStack.scale(2, 2, 1);
-                    
-                    DrawableHelper.drawTextWithShadow(matrixStack, textRenderer, new LiteralText(""+health/2+"X Health    "+hunger/2+"X Food Level"),(int)(width*0.1) , (int)(height*0.35), 0xff0000);
-                    matrixStack.pop();
+            }
+            while(kb.wasPressed()){
+                if (this.kbPressed()) {
                     tickCount = 60f;
                 }
             }
-            
-        });
 
+        });
+    }
+
+    private boolean kbPressed(){
+        final PlayerEntity player = client.player;
+        final InGameHud inGameHud = client.inGameHud;
+        final TextRenderer textRenderer = client.textRenderer;
+        final MatrixStack matrixStack = new MatrixStack();
+        if(player == null) return false;
+
+        
+        double health = client.player.getHealth();
+        double hunger = client.player.getHungerManager().getFoodLevel();
+        int height = client.getWindow().getScaledHeight();
+        int width = client.getWindow().getScaledWidth();
+        matrixStack.push();
+        matrixStack.scale(2, 2, 1);
+        
+        DrawableHelper.drawTextWithShadow(matrixStack, textRenderer, new LiteralText(""+(double)Math.round((health/2)*10)/10+"X Health    "+(double)Math.round((hunger/2)*10)/10+"X Food Level"),(int)(width*0.1) , (int)(height*0.35), 0xff0000);
+        matrixStack.pop();
+        return true;
     }
 
 }
--- health-n-hunger/src/main/java/net/shoaibkhan/health/n/hunger/Initial.java
+++ health-n-hunger/src/main/java/net/shoaibkhan/health/n/hunger/Initial.java
@@ -1,13 +1,32 @@
 package net.shoaibkhan.health.n.hunger;
 
-import net.fabricmc.api.ModInitializer;
+import org.lwjgl.glfw.GLFW;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+
+@Environment(EnvType.CLIENT)
 public class Initial implements ModInitializer {
+    public ClientMod clientMod;
+    public static KeyBinding kb;
 
     @Override
     public void onInitialize() {
         // On initializing minecraft
-        System.out.println("Mod is initializing!!\n\t\tMod created by:-Shoaib Khan");
+        System.out.println("Mod is initializing!!");
+
+        kb = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "Check",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_R,
+            "Easy Life"
+        ));
+        
+        clientMod = new ClientMod(kb);
 
     }
     
